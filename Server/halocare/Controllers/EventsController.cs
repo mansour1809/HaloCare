771d124cc31ff34c09bbde5110e47ddadfa3a538
ZoharLavi <zuish53@gmail.com>
2025-05-11 09:17:40 +0300

    vgi

--- Server/halocare/Controllers/EventsController.cs
+++ Server/halocare/Controllers/EventsController.cs
@@ -56,6 +56,27 @@ namespace halocare.Controllers
             }
         }
 
+        // GET: api/Events/mm-dd-yy
+        [HttpGet("{date}")]
+        public ActionResult<IEnumerable<Event>> GetEventsByDate(DateTime date)
+        {
+            try
+            {
+                List<Event> eventsByDate = _eventService.GetEventByDate(date);
+
+                if (eventsByDate == null)
+                {
+                    return NotFound($"אירוע עם מזהה {date} לא נמצא");
+                }
+
+                return Ok(eventsByDate);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"שגיאה פנימית: {ex.Message}");
+            }
+        }
+
         // GET: api/Events/5/kids
         [HttpGet("{id}/kids")]
         public ActionResult<IEnumerable<Kid>> GetEventKids(int id)
