6322b6d62ae7907854827c11860562e15f7afcc6
mansour1809 <mansour.mothna96@gmail.com>
2025-06-23 21:55:42 +0300

    aaa

--- Client/halocare/src/pages/kids/tretments/TreatmentViewDialog.jsx
+++ Client/halocare/src/pages/kids/tretments/TreatmentViewDialog.jsx
@@ -341,7 +341,7 @@ const TreatmentViewDialog = () => {
             {formError || error}
           </Alert>
         )}
-        
+
         {editMode ? (
           /* editMode */
           <form onSubmit={handleSubmit}>
